60f706d3c94a366502e38fdb8a2a7110ae826dc2
hmuaz <92471130+hmuaz@users.noreply.github.com>
2025-04-01 17:22:01 +0300

    Interpolation

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -6,21 +6,94 @@ public class GamePiece : MonoBehaviour
 {
     public int xIndex;
     public int yIndex;
+
+    public InterpType interpolation = InterpType.SmootherStep;
+    public enum InterpType
+    {
+        Linear,
+        EaseOut,
+        EaseIn,
+        SmoothStep,
+        SmootherStep
+    }; 
+
+    bool m_isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move((int)transform.position.x + 1, (int)transform.position.y, 0.5f);
+        }
     }
 
     public void SetCoord(int x, int y)
     {
         xIndex = x;
         yIndex = y;
+    }
+
+    public void Move(int destX, int destY, float timeToMove)
+    {
+        if(!m_isMoving)
+        StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), timeToMove));
+    }
+
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        Vector3 startPosition = transform.position;
+        bool reachedDestination = false;
+        float elapsedTime = 0f;
+
+        m_isMoving = true;
+        while (!reachedDestination)
+        {
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                reachedDestination = true;
+                transform.position = destination;
+                SetCoord((int)destination.x, (int)destination.y);
+                break;
+            }
+
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp(elapsedTime / timeToMove, 0f, 1f);
+
+            switch (interpolation)
+            {   
+                case InterpType.Linear:
+                    break;
+                case InterpType.EaseOut:
+                    t = Mathf.Sin(t * Mathf.PI * 0.5f);
+                    break;
+                case InterpType.EaseIn:
+                    t = 1 - Mathf.Cos(t * Mathf.PI * 0.5f);
+
+                    break;
+                case InterpType.SmoothStep:
+                    t = t * t * (3 - 2 * t);
+
+                    break;
+                case InterpType.SmootherStep:
+                    t = t * t * t * (t * (6 - 15) + 10);
+                    break;
+                default:
+                    t = t * t * t * (t * (6 - 15) + 10);
+                    break;
+            }
+
+
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+            yield return null;
+        }
+        m_isMoving = false;
+
     }
 }
